583fff97e53c46c245dfcd31805d70465caddbe5
snehathadani <snehacopper@gmail.com>
2022-12-01 18:10:56 -0800

    using Form Data

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -11,7 +11,14 @@ export class AppComponent {
   defaultQuestion = 'teacher'
   answer= "";
   genders =['male', 'female']
-
+user = {
+  username: '',
+  email: '',
+  secretQuestion: '',
+  answer:'',
+  gender:''
+}
+submitted = false
   // onSubmit(form:NgForm){
   //   console.log(form)
   // }
@@ -32,6 +39,11 @@ export class AppComponent {
   }
   
   onSubmit() {
-    console.log("signupForm",this.signUpForm)
+    this.submitted = true
+   this.user.username= this.signUpForm.value.userData.username
+   this.user.email= this.signUpForm.value.userData.email
+   this.user.secretQuestion= this.signUpForm.value.secret
+   this.user.answer= this.signUpForm.value.questionAnswer
+   this.user.gender= this.signUpForm.value.gender
   }
 }
